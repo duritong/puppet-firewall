ee1e2dccf4a374f2df86489da576e646565d6dba
mh <mh@immerda.ch>
2020-08-29 16:01:46 +0200

    fix dispatching

--- manifests/rules/out/puppet.pp
+++ manifests/rules/out/puppet.pp
@@ -6,13 +6,13 @@ class firewall::rules::out::puppet(
   include firewall
   if $firewall::use_nftables {
     class{'nftables::rules::out::puppet':
-      puppetserver      => $puppetmaster,
-      puppetserver_port => $puppetmaster_port,
+      puppetserver      => $puppetserver,
+      puppetserver_port => $puppetserver_port,
     }
   } else {
     class{'shorewall::rules::out::puppet':
-      puppetserver      => $puppetmaster,
-      puppetserver_port => $puppetmaster_port,
+      puppetserver      => $puppetserver,
+      puppetserver_port => $puppetserver_port,
     }
   }
 }
