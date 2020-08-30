0400d62d3db63cb5c13716c565abe980c4bc1555
mh <mh@immerda.ch>
2020-08-30 12:46:18 +0200

    fix class name

--- manifests/rules/smtp_submission.pp
+++ manifests/rules/smtp_submission.pp
@@ -1,5 +1,5 @@
 # manage incoming smtp submission
-class firewall::rules::smtp {
+class firewall::rules::smtp_submission {
   include firewall
   if $firewall::use_nftables {
     include nftables::rules::smtp_submission
