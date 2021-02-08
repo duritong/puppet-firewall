e7534f60aca1a12bbbde453ce5150e23b3c2c825
mh <mh@immerda.ch>
2021-02-08 21:37:12 +0100

    fix + required param

--- manifests/rules/out/munin.pp
+++ manifests/rules/out/munin.pp
@@ -3,9 +3,10 @@ class firewall::rules::out::munin {
   include firewall
   if $firewall::use_nftables {
     nftables::simplerule {
-      'munin-out':
+      'munin_out':
         chain => 'default_out',
-        dport => '4949',
+        dport => 4949,
+        proto => 'tcp',
     }
   } else {
     include shorewall::rules::out::munin
