8edd410506e5616897740d444deb17bd951ad0b8
mh <mh@immerda.ch>
2021-07-05 17:41:31 +0200

    make it easy to dispatch simple incoming rules

---
+++ manifests/rule.pp
@@ -0,0 +1,23 @@
+# manage incoming rule
+define firewall::rule (
+  Nftables::RuleName $rule_name = $title,
+  Nftables::Port $port,
+  Enum['tcp','udp'] $proto,
+) {
+  include firewall
+  if $firewall::use_nftables {
+    nftables::rule {
+      "default_in-${rule_name}":
+        content => "${proto} dport ${port} accept",
+    }
+  } else {
+    shorewall::rule { "net-me-${rule_name}-${proto}":
+      source          => 'net',
+      destination     => '$FW',
+      proto           => $proto,
+      destinationport => String($port),
+      order           => 240,
+      action          => 'ACCEPT';
+    }
+  }
+}
